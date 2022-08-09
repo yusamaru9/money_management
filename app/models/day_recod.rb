bf438424c8dbd42da702e44b736cb11eb0e0423b
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-09 13:53:24 +0000

    [Add] デフォルト値0に設定

--- app/models/day_recod.rb
+++ app/models/day_recod.rb
@@ -5,4 +5,14 @@ class DayRecod < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   
+  attribute :food_cost, :integer, default: 0
+  attribute :commodity, :integer, default: 0
+  attribute :clothing, :integer, default: 0
+  attribute :educate, :integer, default: 0
+  attribute :medical_beauty, :integer, default: 0
+  attribute :transport, :integer, default: 0
+  attribute :socializing, :integer, default: 0
+  attribute :amusement, :integer, default: 0
+  attribute :day_other, :integer, default: 0
+  
 end
--- app/models/month_recod.rb
+++ app/models/month_recod.rb
@@ -2,4 +2,15 @@ class MonthRecod < ApplicationRecord
   
   belongs_to :user
   
+  attribute :brought_forward, :integer, default: 0
+  attribute :income, :integer, default: 0
+  attribute :water_fare, :integer, default: 0
+  attribute :gas_fare, :integer, default: 0
+  attribute :electrical_fare, :integer, default: 0
+  attribute :telephone_fare, :integer, default: 0
+  attribute :deposit, :integer, default: 0
+  attribute :insurance, :integer, default: 0
+  attribute :credit_card, :integer, default: 0
+  attribute :month_other, :integer, default: 0
+  
 end
