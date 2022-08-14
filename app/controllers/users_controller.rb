9dd6bd2536730195e6fa31b656185df329dcfd76
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-14 07:00:07 +0000

    [Add] カレンダー日曜日始まり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  
   def show
     @day_records = DayRecord.all
+  end
+  
+  
+  before_action :set_beginning_of_week
+  
+  private
+  
+  def set_beginning_of_week #日曜日始まり
+    Date.beginning_of_week = :sunday
   end
 end
