efc286f6ead4d0bd16571096da4bb3620be74004
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-15 15:17:14 +0000

    [Add] 一覧ページからのいいねの遷移先・曜日表示

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,13 +4,13 @@ class BookmarksController < ApplicationController
     @day_record = DayRecord.find(params[:day_record_id])
     bookmark = @day_record.bookmarks.new(user_id: current_user.id)
     bookmark.save
-    redirect_to day_record_path(@day_record)
+    redirect_to request.referer
   end
   
   def destroy
     @day_record = DayRecord.find(params[:day_record_id])
     bookmark = @day_record.bookmarks.find_by(user_id: current_user.id)
     bookmark.destroy
-    redirect_to day_record_path(@day_record)
+    redirect_to request.referer
   end
 end
