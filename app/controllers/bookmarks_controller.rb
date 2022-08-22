ed053db6cf4342c062bf936818c334b0efea8b04
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-22 08:18:46 +0000

    [Update] コメントを非同期通信化に変更

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,12 +4,14 @@ class BookmarksController < ApplicationController
     @day_record = DayRecord.find(params[:day_record_id])
     bookmark = @day_record.bookmarks.new(user_id: current_user.id)
     bookmark.save
+    redirect_to request.referer
   end
   
   def destroy
     @day_record = DayRecord.find(params[:day_record_id])
     bookmark = @day_record.bookmarks.find_by(user_id: current_user.id)
     bookmark.destroy
+    redirect_to request.referer
   end
   
 end
--- app/controllers/day_comments_controller.rb
+++ app/controllers/day_comments_controller.rb
@@ -1,16 +1,18 @@
 class DayCommentsController < ApplicationController
   
   def create
-    day_record = DayRecord.find(params[:day_record_id])
+    # 非同期通信化でjsファイルで@day_recordを使用したいから@day_recordを定義する
+    # 同期通信だとローカル変数を定義すればcreateできる
+    @day_record = DayRecord.find(params[:day_record_id])
     day_comment = current_user.day_comments.new(post_comment_params)
-    day_comment.day_record_id = day_record.id
+    day_comment.day_record_id = @day_record.id
     day_comment.save
-    redirect_to day_record_path(day_record)
   end
   
   def destroy
     DayComment.find(params[:id]).destroy
-    redirect_to day_record_path(params[:day_record_id])
+    # 非同期通信化でjsファイルで@day_recordを使用したいから@day_recordを定義する
+    @day_record = DayRecord.find(params[:day_record_id])
   end
   
   private
