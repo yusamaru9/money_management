58d8d341ac76195974489085235a23e8a3142918
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-22 05:01:28 +0000

    [Update] 詳細ページのブックマーク機能を非同期通信化

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,15 @@
 class BookmarksController < ApplicationController
   
-  # before_action :ensure_correct_bookmark
-  
   def create
     @day_record = DayRecord.find(params[:day_record_id])
     bookmark = @day_record.bookmarks.new(user_id: current_user.id)
     bookmark.save
-    redirect_to request.referer
   end
   
   def destroy
     @day_record = DayRecord.find(params[:day_record_id])
     bookmark = @day_record.bookmarks.find_by(user_id: current_user.id)
     bookmark.destroy
-    redirect_to request.referer
   end
   
 end
