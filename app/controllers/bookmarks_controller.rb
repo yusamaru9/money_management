985961481ae5404c02e7c7c79889a94d392407e5
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-22 10:48:56 +0000

    [Update] ブックマーク一覧からブックマークするの非同期

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,14 +2,15 @@ class BookmarksController < ApplicationController
   
   def create
     @day_record = DayRecord.find(params[:day_record_id])
-    bookmark = @day_record.bookmarks.new(user_id: current_user.id)
-    bookmark.save
+    # jsファイル読み込むためにbookmarkをインスタンス変数（@bookmark）にする（destroyも同様）
+    @bookmark = @day_record.bookmarks.new(user_id: current_user.id)
+    @bookmark.save
   end
   
   def destroy
     @day_record = DayRecord.find(params[:day_record_id])
-    bookmark = @day_record.bookmarks.find_by(user_id: current_user.id)
-    bookmark.destroy
+    @bookmark = @day_record.bookmarks.find_by(user_id: current_user.id)
+    @bookmark.destroy
   end
   
 end
