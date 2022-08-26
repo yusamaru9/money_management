1937fe51ad80957272ee949df940a277d395d1e1
yusamaru9 <106299497+yusamaru9@users.noreply.github.com>
2022-08-26 03:13:02 +0000

    [Fix] 新規登録のバリデーションエラーメッセージの順番替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  validates :name, presence: true #deviseの上に置くことでバリデーションエラーメッセージの順番を名前が上にくる
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable #ここにdeviseのバリデーションがある
          
   has_many :month_records, dependent: :destroy
   has_many :day_records, dependent: :destroy
   has_many :day_comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   
-  validates :name, presence: true
-  validates :email, presence: true
-  # validates :password, presence: true, length: { minimum: 6 }
-  # validates :password, presence: true, format: { with: VALID_PASSWORD_REGIX }
   
   def self.guest
     find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
